bc29a4e203b70b7b1d81d706023e220c444a569e
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-11-13 00:12:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ window.onscroll = function () {
 function scrollFunction() {
     var scrollToTopBtn = document.getElementById("scrollToTopBtn");
     var proyectosSection = document.getElementById("proyectos");
+    var header = document.getElementById("myHeader");
 
     // Obtén la posición de la parte inferior de la sección de proyectos
     var proyectosBottom = proyectosSection.offsetTop + proyectosSection.offsetHeight;
@@ -23,38 +24,84 @@ function scrollFunction() {
         document.documentElement.scrollTop > proyectosBottom
     ) {
         scrollToTopBtn.style.display = "block";
+        header.classList.add("hidden"); // Agrega la clase "hidden" para ocultar suavemente la barra de navegación
     } else {
         scrollToTopBtn.style.display = "none";
+        header.classList.remove("hidden"); // Elimina la clase "hidden" para mostrar suavemente la barra de navegación
+    }
+
+    // Ajuste adicional para ocultar la barra de navegación al ver la sección de proyectos
+    var proyectosTop = proyectosSection.offsetTop;
+
+    if (
+        document.body.scrollTop > proyectosTop ||
+        document.documentElement.scrollTop > proyectosTop
+    ) {
+        header.classList.add("hidden");
+    } else {
+        header.classList.remove("hidden");
     }
 }
 
+// Restaurar la barra de navegación cuando se hace clic en el botón de subir
+document.getElementById("scrollToTopBtn").addEventListener("click", function () {
+    var header = document.getElementById("myHeader");
+    header.classList.remove("hidden"); // Elimina la clase "hidden" para mostrar suavemente la barra de navegación
+});
+/**zoomeo */
+document.getElementById("miFoto").addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)";
+});
+;
 
 
-/*burbuja*/
-document.addEventListener("DOMContentLoaded", function () {
-    centrarBurbuja();
-    // Evento para recalcular la posición al cambiar el tamaño de la ventana
-    window.addEventListener("resize", centrarBurbuja);
+/**animacion */
+var foto = document.getElementById("miFoto");
+
+foto.addEventListener("mouseover", function() {
+    anime({
+        targets: '#miFoto',
+        scale: 1.2,
+        duration: 500,
+        easing: 'easeInOutQuad'
+    });
 });
 
-function centrarBurbuja() {
-    const burbuja = document.querySelector(".burbuja");
-    // Centrar la burbuja en la mitad de la pantalla
-    const leftPos = window.innerWidth / 2 - burbuja.offsetWidth / 2;
-    const topPos = window.innerHeight / 2 - burbuja.offsetHeight / 2;
-    burbuja.style.left = `${leftPos}px`;
-    burbuja.style.top = `${topPos}px`;
-}
-  /*foto inicio*/
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const foto = document.getElementById("miFoto");
-
-    foto.addEventListener("mouseenter", function () {
-        foto.classList.add("zoomed");
+foto.addEventListener("mouseout", function() {
+    anime({
+        targets: '#miFoto',
+        scale: 1,
+        duration: 500,
+        easing: 'easeInOutQuad'
     });
+});
 
-    foto.addEventListener("mouseleave", function () {
-        foto.classList.remove("zoomed");
+/**objetivos */
+$(document).ready(function() {
+    // Contador para asignar identificadores únicos a los campos
+    var contadorObjetivos = 1;
+
+    // Agregar un campo de objetivo al cargar la página
+    agregarCampoObjetivo();
+
+    // Función para agregar un nuevo campo de objetivo
+    function agregarCampoObjetivo() {
+        var nuevoCampo = `<div class="objetivo" id="objetivo${contadorObjetivos}">
+                            <textarea placeholder="Escribe tu objetivo aquí"></textarea>
+                            <button class="eliminarObjetivo" onclick="eliminarCampoObjetivo(${contadorObjetivos})">Eliminar</button>
+                        </div>`;
+
+        $("#objetivosContainer").append(nuevoCampo);
+        contadorObjetivos++;
+    }
+
+    // Función para eliminar un campo de objetivo
+    window.eliminarCampoObjetivo = function(idObjetivo) {
+        $(`#objetivo${idObjetivo}`).remove();
+    };
+
+    // Agregar evento al botón para agregar objetivos
+    $("#agregarObjetivo").on("click", function() {
+        agregarCampoObjetivo();
     });
 });
