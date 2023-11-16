265c0ccc6a9bcdffc5055f54b10e22a44ef21a8d
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-11-16 21:23:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-    // Función para desplazarse al principio de la página
+   
+   
+   
+   
+   
+   // Función para desplazarse al principio de la página
     function scrollToTop() {
         window.scrollTo({
             top: 0,
@@ -45,18 +50,7 @@
     // Almacena la altura del header
     var headerHeight = document.querySelector('header').offsetHeight;
 
-    // Función para manejar el evento de desplazamiento
-    function handleScroll() {
-        // Obtiene la posición actual del desplazamiento
-        var scrollPosition = window.scrollY || window.pageYOffset;
-
-        // Aplica la clase header-hidden si el desplazamiento es mayor que la altura del header
-        if (scrollPosition > headerHeight) {
-            document.querySelector('header').classList.add('header-hidden');
-        } else {
-            document.querySelector('header').classList.remove('header-hidden');
-        }
-    }
+   
 
     // Agrega el evento de desplazamiento al documento
     document.addEventListener('scroll', handleScroll);
@@ -74,45 +68,7 @@ document.getElementById("miFoto").addEventListener("mouseout", function() {
 
 
 /**animacion */
-var foto = document.getElementById("miFoto");
 
-foto.addEventListener("mouseover", function() {
-    anime({
-        targets: '#miFoto',
-        scale: 1.2,
-        duration: 500,
-        easing: 'easeInOutQuad'
-    });
-});
-
-foto.addEventListener("mouseout", function() {
-    anime({
-        targets: '#miFoto',
-        scale: 1,
-        duration: 500,
-        easing: 'easeInOutQuad'
-    });
-});
-
-/**objetivos */
-$(document).ready(function() {
-    // Contador para asignar identificadores únicos a los campos
-    var contadorObjetivos = 1;
-
-    // Agregar un campo de objetivo al cargar la página
-    agregarCampoObjetivo();
-
-
-    // desaparece barra nav
-    window.eliminarCampoObjetivo = function(idObjetivo) {
-        $(`#objetivo${idObjetivo}`).remove();
-    };
-
-    // Agregar evento al botón para agregar objetivos
-    $("#agregarObjetivo").on("click", function() {
-        agregarCampoObjetivo();
-    });
-});
 
 
 // Función para manejar el evento de desplazamiento
@@ -130,65 +86,6 @@ function handleScroll() {
     document.querySelector('header').style.opacity = opacity;
 }
 
-// Agrega el evento de desplazamiento al documento
-document.addEventListener('scroll', handleScroll);
-/**optimizacion */
-/**optimizacion */
-$(document).ready(function() {
-    var header = $('#myHeader');
-    var headerHeight = header.outerHeight();
-
-    // Función para manejar el evento de desplazamiento
-    function handleScroll() {
-        var scrollPosition = $(window).scrollTop();
-        var opacity = Math.max(0, 1 - (scrollPosition / headerHeight));
-
-        // Utilizar una transición para suavizar el cambio de opacidad
-        header.css('transition', 'opacity 0.3s ease-in-out');
-        header.css('opacity', opacity.toFixed(2));
-    }
-
-    // Agregar transición suave al cargar la página
-    handleScroll();
-
-    $(window).scroll(handleScroll);
-
-    // ... (resto del código)
-});
-
-
-/**UL */
-document.addEventListener('DOMContentLoaded', function() {
-    // Array de objetivos
-    const objetivosArray = [
-        "Desarrollar nuevas habilidades",
-        "Contribuir al éxito del equipo",
-        "Aprender continuamente",
-        // Puedes agregar más objetivos según sea necesario
-    ];
-
-    // Obtener la lista de objetivos
-    const objetivosList = document.getElementById('objetivosList');
-
-    // Crear elementos de lista y agregarlos a la lista
-    objetivosArray.forEach(function(objetivo) {
-        const listItem = document.createElement('li');
-        listItem.classList.add('objetivo');
-        listItem.textContent = objetivo;
-        objetivosList.appendChild(listItem);
-    });
-});
-/**fotos animacion-desaparicion */
-
-    var imagenContainer = document.getElementById('imagenContainer');
-
-    imagenContainer.addEventListener('mouseenter', function () {
-        imagenContainer.classList.add('foto-container');
-    });
-
-    imagenContainer.addEventListener('mouseleave', function () {
-        imagenContainer.classList.remove('foto-container');
-    });
 
 
 
